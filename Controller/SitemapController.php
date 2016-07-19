89150b21000fc71fc7f417c0d9d0d2a5790f7adc
Jona <jona303@gmail.com>
2016-07-19 14:27:22 +0200

    sitemap.xml

--- Controller/SitemapController.php
+++ Controller/SitemapController.php
@@ -4,7 +4,7 @@ namespace SKCMS\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class SitemapController extends Controller
 {
     public function sitemapAction()
     {
