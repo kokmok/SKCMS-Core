f9f197de8fae8d21a63f20f52a54eaea3698be4c
Jona <jona@solid-kiss.be>
2017-06-20 14:19:57 +0200

    adding test email command

---
+++ Command/TestMailCommand.php
@@ -0,0 +1,32 @@
+<?php
+namespace SKCMS\CoreBundle\Command;
+
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class TestMailCommand extends ContainerAwareCommand
+{
+    const ARGUMENT_EMAIL = "ARGUMENT_EMAIL";
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('skcms_core:test_mail')
+            ->addArgument(self::ARGUMENT_EMAIL,InputArgument::REQUIRED,'',"jona@solid-kiss.be")
+            ->setDescription('Hello PhpStorm');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $message = $this->getContainer()->get('mailer')->createMessage();
+        
+    }
+}
